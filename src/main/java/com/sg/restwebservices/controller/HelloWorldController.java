62ce92460ea0d361795f15a793f9244ff807ebf6
Sanu <sanu.india@gmail.com>
2023-12-15 10:22:17 -0500

    add from Git to Hello world

--- src/main/java/com/sg/restwebservices/controller/HelloWorldController.java
+++ src/main/java/com/sg/restwebservices/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("hello-world")
     public static String helloWorld(){
-        return "Hello World";
+        return "Hello World - From Git";
     }
 }
