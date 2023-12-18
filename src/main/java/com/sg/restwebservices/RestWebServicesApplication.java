df2d1b0356f8934e90fd9ed0b20d429e3382a0e4
Sanu <sanu.india@gmail.com>
2023-12-18 17:53:49 -0500

    adjested to handle tomcat deployment

--- src/main/java/com/sg/restwebservices/RestWebServicesApplication.java
+++ src/main/java/com/sg/restwebservices/RestWebServicesApplication.java
@@ -2,9 +2,10 @@ package com.sg.restwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestWebServicesApplication {
+public class RestWebServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWebServicesApplication.class, args);
