751e603a7c308beb12f8bcce0f6d56ab5ca8dc34
Sanu <sanu.india@gmail.com>
2023-12-18 18:44:43 -0500

    adjusted to handle tomcat deployment

--- src/main/java/com/sg/restwebservices/controller/HelloWorldController.java
+++ src/main/java/com/sg/restwebservices/controller/HelloWorldController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
+    static int count = 0;
     @GetMapping("hello-world")
     public static String helloWorld(){
-        return "Hello World - From Git";
+        return "Hello World - From Git" + count++;
     }
 }
